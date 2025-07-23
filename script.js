b6035de77e139c0c49b5def18fb9d88fc4b8460f
Jhon Cavalli <jhoncavalli@hotmail.com>
2025-07-23 18:27:38 -0300

    add const e selector nas id e class loading, result e error

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let form = document.getElementById("ConvertForm");
+const form = document.getElementById("ConvertForm");
+const amount = document.getElementById("amount");
+const fromCurrency = document.getElementById("fromCurrency");
+const convertedAmount = document.getElementById("convertedAmount");
+const toCurrency = document.getElementById("toCurrency");
+const loading = document.querySelector("loading");
+const result = document .querySelector(".result");
+const error = document.querySelector(".error");
+
+
 
     function convertMoney() {
         console.log("Converting money...");
