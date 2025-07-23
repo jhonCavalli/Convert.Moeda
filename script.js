cb49e54054ef6285f2e4539546a117324b98fd59
Jhon Cavalli <jhoncavalli@hotmail.com>
2025-07-23 20:23:33 -0300

    foram corrigiondo alguns bug do projeto, por causa de erro de codigo

--- script.js
+++ script.js
@@ -2,46 +2,54 @@ const form = document.getElementById("ConvertForm");
 const amount = document.getElementById("amount");
 const fromCurrency = document.getElementById("fromCurrency");
 const toCurrency = document.getElementById("toCurrency");
-const convertedAmount = document.getElementById("convertedAmout"); // corrigido nome
+const convertedAmount = document.getElementById("convertedAmout");
 const loading = document.querySelector(".loading");
 const result = document.querySelector(".result");
 const error = document.querySelector(".error");
 
-const API_URL = "https://v6.exchangerate-api.com/v6/5d4653edf286154e58f67721/latest/";
 
-async function convertMoney() {
-    loading.style.display = "block";
-    error.style.display = "none";
-    result.style.display = "none";
 
-    try {
-        const response = await fetch(API_URL + fromCurrency.value);
-        const data = await response.json();
+const API_URL =" https://v6.exchangerate-api.com/v6/5d4653edf286154e58f67721/latest/";
 
-        const rate = data.rates[toCurrency.value];
-        const convertedRate = (amount.value * rate).toFixed(2);
+   async function convertMoney() {
+        loading.style.display = "block";
+        error.style.display = "none";
+        result.style.display = "none";
+        
 
-        convertedAmount.value = convertedRate;
+        try {
+            const response = await fetch(API_URL + fromCurrency.value); 
+            const data = await response.json();
 
-        result.style.display = "block";
-        result.innerHTML = `
-            <div style="font-size: 1.4rem;">
-                ${amount.value} ${fromCurrency.value} = ${convertedRate} ${toCurrency.value}
-            </div>
-            <div style="font-size: 1.2rem; opacity: 0.8; margin-top: 0.5rem;">
-                Taxa: 1 ${fromCurrency.value} = ${rate} ${toCurrency.value}
-            </div>
-        `;
-    } catch (err) {
-        error.style.display = "block";
-        error.textContent = "Erro ao converter moeda. Tente novamente mais tarde.";
-    } finally {
-        loading.style.display = "none";
+
+            const rate = data.conversion_rates[toCurrency.value];
+            const convertedRate = (amount.value * rate).toFixed(2);
+
+            convertedAmount.value = convertedRate;
+
+            result.style.display = "block";
+            result.innerHTML = `
+                <div style="fonte-size:1.4rem;>
+                    &{amount.value} ${fromCurrency.value} = ${convertedRate} ${toCurrency.value}.
+                </div>
+                <div style="font-size:1.2rem opacity:0.8; martin-top:0.5rem;">
+                    Taxa: 1 ${fromCurrency.value} = ${rate} ${toCurrency.value}
+                </div>
+            `;
+
+
+        }
+        catch(error) {
+            alert("Erro ao converter moeda. Por favor, tente novamente mais tarde.");
+
+        }
+        finally {
+            loading.style.display = "none";
+        }
     }
-}
 
-form.addEventListener("submit", function (event) {
+
+form.addEventListener("submit", function(event) {
     event.preventDefault();
     convertMoney();
 });
-
