e13be416ec61616161f2f242719a3d52c3c5c954
Jhon Cavalli <jhoncavalli@hotmail.com>
2025-07-23 18:44:33 -0300

    add url de API na const

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const form = document.getElementById("ConvertForm");
 const amount = document.getElementById("amount");
 const fromCurrency = document.getElementById("fromCurrency");
-const convertedAmount = document.getElementById("convertedAmount");
 const toCurrency = document.getElementById("toCurrency");
+const convertedAmount = document.getElementById("convertedAmount");
 const loading = document.querySelector("loading");
 const result = document .querySelector(".result");
 const error = document.querySelector(".error");
 
 
 
+const API_URL =" https://v6.exchangerate-api.com/v6/5d4653edf286154e58f67721/latest/";
+
     function convertMoney() {
-        console.log("Converting money...");
+        loading.style.display = "block";
+
+
     }
 
 
