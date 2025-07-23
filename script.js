2020d6c297b712de6c6b47969a7f4027e2a4f754
Jhon Cavalli <jhoncavalli@hotmail.com>
2025-07-23 19:30:15 -0300

    add grandiente n result e no error

--- script.js
+++ script.js
@@ -2,18 +2,30 @@ const form = document.getElementById("ConvertForm");
 const amount = document.getElementById("amount");
 const fromCurrency = document.getElementById("fromCurrency");
 const toCurrency = document.getElementById("toCurrency");
-const convertedAmount = document.getElementById("convertedAmount");
-const loading = document.querySelector("loading");
-const result = document .querySelector(".result");
+const convertedAmout = document.getElementById("convertedAmout");
+const loading = document.querySelector(".loading");
+const result = document.querySelector(".result");
 const error = document.querySelector(".error");
 
 
 
 const API_URL =" https://v6.exchangerate-api.com/v6/5d4653edf286154e58f67721/latest/";
 
-    function convertMoney() {
+   async function convertMoney() {
         loading.style.display = "block";
+        
 
+        try {
+            const response = await fetch(API_URL + fromCurrency.value); 
+            const data = await response.json();
+
+            const rate = data.rates[toCurrency.value];
+            const convertedRate = (amount.value * rate).toFixed(2);
+            
+        }
+        catch(error) {
+            alert("Erro ao converter moeda. Por favor, tente novamente mais tarde.");
+        }
 
     }
 
